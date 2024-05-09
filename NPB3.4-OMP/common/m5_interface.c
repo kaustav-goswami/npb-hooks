b64b861070811a058f3d2210282767e912c6cd0d
Kaustav Goswami <kggoswami@ucdavis.edu>
2024-05-09 19:32:04 +0000

    updated to use mmap interface
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

--- NPB3.4-OMP/common/m5_interface.c
+++ NPB3.4-OMP/common/m5_interface.c
@@ -1,5 +1,17 @@
 #include <gem5/m5ops.h>
+#include <m5_mmap.h>
 #include <stdlib.h>
+
+void init_() {
+    // Has to be called using addr as we are using kvm to boot the CPUs
+    m5op_addr = 0x10010000;
+    map_m5_mem();
+}
+
+void uninit_() {
+    // Unlikely that the program will reach here
+    unmap_m5_mem();
+}
 
 void m5_exit_interface_() {
     m5_exit(0);
